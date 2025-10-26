6d4f088c2923fbe167062a1ec0976465eb57ead0
Juan <s22120254@alumnos.itsur.edu.mx>
2025-10-26 14:24:45 -0600

    Agrega notificaciones programadas para agendas
    Incluye:
    - Creación de canal de notificaciones
    - Solicitud de permiso POST_NOTIFICATIONS
    - Función showTestNotification()
    - Se creo la carpeta Drawable en res para el icono de las notificaciones ic_notification

--- app/src/main/java/com/dev/agenda_movil/MainActivity.kt
+++ app/src/main/java/com/dev/agenda_movil/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.dev.agenda_movil
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import com.dev.agenda_movil.ui.theme.Agenda_movilTheme
 import com.dev.uiElements.RecordatoriosScreen
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
 
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // El usuario concedió el permiso
+                showTestNotification()
+            } else {
+                // El usuario lo negó
+                Toast.makeText(this, "No se otorgó permiso para notificaciones", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            when {
+                checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED -> {
+                    // Ya tienes permiso
+                    showTestNotification()
+                }
+                else -> {
+                    // Todavía no tienes permiso → pedirlo
+                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+        } else {
+
+            showTestNotification()
+        }
+
         setContent {
             Agenda_movilTheme {
                 Surface(
@@ -26,5 +66,48 @@ class MainActivity : ComponentActivity() {
 
             }
         }
+        createNotificationChannel()
     }
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "recordatorios_channel"
+            val name = "Recordatorios"
+            val descriptionText = "Canal para notificaciones de recordatorios"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+
+            // Registrar el canal en el sistema
+            val notificationManager: NotificationManager =
+                getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showTestNotification() {
+        val builder = NotificationCompat.Builder(this, "recordatorios_channel")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("Prueba de notificación")
+            .setContentText("canal de notificaciones funciona correctamente")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)
+            == PackageManager.PERMISSION_GRANTED) {
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(1, builder.build())
+            }
+
+        } else {
+            Toast.makeText(this, "Permiso de notificaciones no concedido", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+
+
 }
